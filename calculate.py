e74e760e64912d7db03fed9f3c3c34d7db23e991
Zhou Yaoyang <diamondzyy@sina.com>
2016-11-22 16:40:15 +0800

    calculate predicted performance

---
+++ calculate.py
@@ -0,0 +1,10 @@
+with open('./x') as f:
+    b = f.readline()
+    base = int(b.split('         ')[1])
+    b = f.readline()
+    wait = int(b.split('         ')[1])
+    b = f.readline()
+    miss = int(b.split('         ')[1])
+
+    print "calculated performance: ",
+    print float(base+miss)/(base+miss+wait)
