148825daaddd58cfa7e192f95bc9f87924230657
Zhou Yaoyang <diamondzyy@sina.com>
2017-02-02 21:02:08 +0800

    add misprection rate computing

--- check_slots.py
+++ check_slots.py
@@ -6,14 +6,37 @@ parser = argparse.ArgumentParser(description='check sum of slots')
 parser.add_argument('stats', metavar='stats',
                     help='stats.txt to be converted')
 
+parser.add_argument('tail', metavar='tail',
+                    help='use statistics in tail of stats.txt')
+
 args = parser.parse_args()
 
 # cmd = 'tail ' + args.stats + ' -n 100 | grep "QoS\|committedInsts::\|Slots::0\|ipc::\|HPT\|numCycles\|Overlapped"'
 
-x = sh.grep(sh.grep("system.cpu.committedInsts::0 *200[0-9]\{6\}",
-                    args.stats, '-m', "1", '-A', '600', '-B', '600'),
-            "QoS\|committedInsts::\|Slots::0\|ipc::\|HPT\|numCycles\|Overlapped\
-            \|rectified_waits")
+targets = [
+    'committedInsts::0',
+    'Slots::0',
+    'ipc::',
+    'HPT',
+    'numCycles',
+    'Overlapped',
+    'rectified',
+    'wait_to_miss::0',
+    'system.cpu.iew.exec_branches::0',
+    'system.cpu.iew.branchMispredicts::0',
+]
+
+tstr = 'Qos'
+for s in targets:
+    tstr += '\|'+s
+
+if args.tail == 'true':
+    x = sh.grep(sh.tail(args.stats, '-n', '1000'), tstr)
+else:
+    x = sh.grep(sh.grep("system.cpu.committedInsts::0 *200[0-9]\{6\}",
+                        args.stats, '-m', "1", '-A', '600', '-B', '600'), tstr)
+
+print tstr
 
 print x
 
@@ -31,6 +54,10 @@ for line in x:
         d['system.cpu.fmt.numMissSlots::0'] = line.split()[1]
     elif line.startswith('system.cpu.ipc::0'):
         d['system.cpu.ipc::0'] = line.split()[1]
+    elif line.startswith('system.cpu.iew.exec_branches::0'):
+        d['hptBranch'] = line.split()[1]
+    elif line.startswith('system.cpu.iew.branchMispredicts::0'):
+        d['hptMiss'] = line.split()[1]
 
 base = float(d['system.cpu.fmt.numBaseSlots::0'])
 wait = float(d['system.cpu.fmt.numWaitSlots::0'])
@@ -45,3 +72,9 @@ qos = (base + miss) / (base + miss + wait)
 print "Predicted QoS:", qos
 
 print 'Predicted ST IPC:', ipc/qos
+
+branch = float(d['hptBranch'])
+branch_miss = float(d['hptMiss'])
+
+print "Branch misprediction rate:", branch_miss / branch
+
