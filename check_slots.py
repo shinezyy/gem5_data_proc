5432ca77192b22b2aaccbd90eb3892e7dc8bbfb2
Zhou Yaoyang <diamondzyy@sina.com>
2017-03-07 09:25:24 +0800

    print slots in stages

--- check_slots.py
+++ check_slots.py
@@ -15,30 +15,44 @@ args = parser.parse_args()
 
 targets = [
     'committedInsts::0',
+    #'committedInsts::1',
     'Slots::0',
     'ipc::',
-    'HPT',
+    #'HPT',
     'numCycles',
-    'Overlapped',
-    'rectified',
-    'wait_to_miss::0',
-    'system.cpu.iew.exec_branches::0',
-    'system.cpu.iew.branchMispredicts::0',
+    #'Overlapped',
+    #'rectified',
+    #'wait_to_miss::0',
+    #'system.cpu.iew.exec_branches::0',
+    #'system.cpu.iew.branchMispredicts::0',
+    #'system.cpu.rename.*Cycles::0',
+    #'system.cpu.itb.fetch_misses',
+    #'system.cpu.itb.fetch_accesses',
+    'system.cpu.*_Slots',
+    'system.cpu.rename.*Full',
+    'system.cpu.*_utilization',
+    'overall_miss_rate',
 ]
+
+def shorter(x):
+    for line in str(x).split('\n'):
+        first_half = line.split('#')[0].rstrip(' ')
+        print first_half
+
 
 tstr = 'Qos'
 for s in targets:
     tstr += '\|'+s
 
 if args.tail == 'true':
-    x = sh.grep(sh.tail(args.stats, '-n', '1000'), tstr)
+    x = sh.grep(sh.tail(args.stats, '-n', '2000'), tstr)
 else:
-    x = sh.grep(sh.grep("system.cpu.committedInsts::0 *200[0-9]\{6\}",
-                        args.stats, '-m', "1", '-A', '600', '-B', '600'), tstr)
+    x = sh.grep(sh.grep("system.cpu.committedInsts::0 *2[0-9]\{8\}",
+                        args.stats, '-m', "1", '-A', '1000', '-B', '600'), tstr)
 
 print tstr
 
-print x
+shorter(x)
 
 d = dict()
 
@@ -58,6 +72,10 @@ for line in x:
         d['hptBranch'] = line.split()[1]
     elif line.startswith('system.cpu.iew.branchMispredicts::0'):
         d['hptMiss'] = line.split()[1]
+    elif line.startswith('system.cpu.itb.fetch_accesses::0'):
+        itb_all = float(line.split()[1])
+    elif line.startswith('system.cpu.itb.fetch_misses::0'):
+        itb_miss = float(line.split()[1])
 
 base = float(d['system.cpu.fmt.numBaseSlots::0'])
 wait = float(d['system.cpu.fmt.numWaitSlots::0'])
@@ -73,8 +91,32 @@ print "Predicted QoS:", qos
 
 print 'Predicted ST IPC:', ipc/qos
 
+# print 'ITLB miss rate:', float(itb_miss) / itb_all
+
+#  branch misPrediction Rate
+'''
 branch = float(d['hptBranch'])
 branch_miss = float(d['hptMiss'])
-
 print "Branch misprediction rate:", branch_miss / branch
+'''
+
+print ''
+
+# rename cycles
+
+'''
+for line in str(x).split('\n'):
+    if line.startswith('system.cpu.rename.'):
+        first_half = line.split('#')[0].rstrip(' ')
+        number = first_half.split(' ')[-1]
+        print number
+'''
+
+
+
+
+
+
+
+
 
