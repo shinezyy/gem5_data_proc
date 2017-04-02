3f15aa72b9be5fc96883d223ea7a3bd8fdf25a16
Zhou Yaoyang <diamondzyy@sina.com>
2017-04-02 22:57:24 +0800

    ..

--- extract.py
+++ extract.py
@@ -81,9 +81,9 @@ def extract_stat(stat_file, use_tail, st_stat_file, num_insts=0, brief = False):
         raw_str = sh.grep("system.cpu.committedInsts::0 *2[0-9]\{8\}",
                         stat_file, '-m', "1", '-A', '1000', '-B', '600')
     else:
-        raw_str = sh.grep("system.cpu.committedInsts::0 *" + str(num_insts) +
-                          "[0-9]\{6\}",
-                        stat_file, '-m', "1", '-A', '1000', '-B', '600')
+        raw_str = sh.grep("\"system.cpu.committedInsts::0 *" +\
+                          str(num_insts) + "[0-9]\{7\} \"",
+                          stat_file, '-m', "1", '-A', '1000', '-B', '600')
 
     d = dict()
 
@@ -112,8 +112,9 @@ def extract_stat(stat_file, use_tail, st_stat_file, num_insts=0, brief = False):
 
         for x in range(msd - 10, msd + 10):
             try:
-                first = sh.grep("system.cpu.committedInsts::0" +
-                    " *{}".format(x) + "[0-9]\{6\}",
+                t = " *{}".format(x) + "[0-9]\{" + str(num_digits-3) + "\} "
+                # print t
+                first = sh.grep("system.cpu.committedInsts::0" + t,
                     st_stat_file, '-m', "1", '-A', '1000', '-B', '600')
             except:
                 continue
