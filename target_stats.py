358ec341747b190f496e4ea37248ad71bdc92865
Zhou Yaoyang <diamondzyy@sina.com>
2017-04-30 11:34:06 +0800

    seperate target stats

--- target_stats.py
+++ target_stats.py
@@ -1,6 +1,7 @@
 standard_targets = [
     '(numCycles)',
     '(rename\..+)_Slots',
+    'rename\.(.+FullEvents::0)',
     '(iew\..+)_Slots',
     'cpu\.(committedInsts::\d)',
     'fmt\.(num.*Slots::0)',
@@ -8,14 +9,23 @@ standard_targets = [
     'cpu\.(HPTpredIPC)::0',
     '(cpu\.HPTQoS)',
     'fmt\.(mlp_rectification)',
-    # 'iew.(branchMispredicts::0)',
-    # 'iew.exec_(branches::0)',
-    'rename\.(.+FullEvents::0)',
+]
+
+cache_targets = [
     '(dcache.*_m)iss_rate::0',
+    '(icache.*_m)iss_rate::0',
     '(l2.*_m)iss_rate::0',
-    '(iq_util)ization::[0]',
-    '(rob_util)ization::[0]',
-    'lsq\.(.+_util)ization::[0]',
+]
+
+branch_targets = [
+    'iew.(branchMispredicts::0)',
+    'iew.exec_(branches::0)',
+]
+
+util_targets = [
+    '(iq_util)ization::0',
+    '(rob_util)ization::0',
+    'lsq\.(.+_util)ization::0',
 ]
 
 brief_targets = [
