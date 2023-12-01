4c0d99a59be3d65f03c14ba25268d81e92c74a30
Zhou Yaoyang <zhouyaoyang17@mails.ucas.edu.cn>
2023-12-01 14:58:22 +0800

    Sort by weighted performance diff

--- points/find_max_diff_point.py
+++ points/find_max_diff_point.py
@@ -12,9 +12,12 @@ parser.add_argument('-b', '--bmk', help='csv file 2 to sort')
 args = parser.parse_args()
 
 js = json.load(open(args.weight_json))
+df1 = pd.read_csv(args.csv1, index_col=0)
+df2 = pd.read_csv(args.csv2, index_col=0)
+intersection = set(list(df1.index)).intersection(set(list(df2.index)))
 points = [
-    pd.read_csv(args.csv1, index_col=0),
-    pd.read_csv(args.csv2, index_col=0),
+    df1.loc[list(intersection)],
+    df2.loc[list(intersection)],
 ]
 
 def get_related_points_on_wl(workload, points):
@@ -25,18 +28,25 @@ def add_cpi(points):
     points['cpi'] = 1.0 / points['ipc']
 
 def add_weighted_cycles(points_df, js, wokload):
-    for point in js[wokload]['points']:
+    print(js[wokload])
+    for point in list(points_df['point']):
         wl_point = f'{wokload}_{point}'
         points_df.loc[wl_point, 'weighted_cycles'] = \
-            float(js[wokload]['points'][point]) * points_df.loc[wl_point, 'cpi']
-    
+            float(js[wokload]['points'][str(point)]) * \
+                points_df.loc[wl_point, 'cpi']
+
 weighted_cycles = []
 for i in range(2):
     add_cpi(points[i])
+    print(points[i])
     add_weighted_cycles(points[i], js, args.bmk)
     df = get_related_points_on_wl(args.bmk, points[i])
-    print(df)
     weighted_cycles.append(df['weighted_cycles'])
-diff = weighted_cycles[0] - weighted_cycles[1]
-diff = diff.sort_values(ascending=True)
+
+diff = pd.DataFrame((weighted_cycles[0] - weighted_cycles[1])*100)
+diff['csv1 ipc'] = points[0]['ipc']
+diff['csv2 ipc'] = points[1]['ipc']
+
+diff = diff.sort_values(by=['weighted_cycles'], ascending=False)
 print(diff)
+
